b149b363d437e16cb9087c2eee33dddff665f82d
Bradman28 <bsurinak28@gmail.com>
2024-08-12 23:44:05 -0500

    Created Controller class

---
+++ quote_api/src/main/java/com/surinak/quote_api/hello/Hello.java
@@ -0,0 +1,21 @@
+package com.surinak.quote_api.hello;
+
+// importing the different frameworks
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Hello {
+
+    @RequestMapping("/hello")
+    public String hello(
+        @RequestParam(name="user", required=false, defaultValue="Champ")
+        String user, 
+        Model model) {
+        model.addAttribute("user", user);
+        return "hello";
+    }
+    
+}
--- quote_api/src/main/java/com/surinak/quote_api/hello/QuoteApiApplication.java
+++ quote_api/src/main/java/com/surinak/quote_api/hello/QuoteApiApplication.java
@@ -1,4 +1,4 @@
-package com.surinak.quote_api;
+package com.surinak.quote_api.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
